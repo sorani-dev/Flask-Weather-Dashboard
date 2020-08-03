28c54d1401b24c2c6cd9d832350d4196367c38ac
simon441 <simon.cateau@yahoo.fr>
2020-08-03 17:12:30 +0200

    Set up the Flask dashboard website for user input

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-import requests
 import configparser
-import os
+
+import requests
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def weather_dashboard():
+    return render_template('home.html')
+
+
+@app.route("/results", methods=['POST'])
+def render_results():
+    print( "Results")
+    zip_code = request.form['zip_code']
+    country = request.form['country']
+    print(zip_code, country)
+    return {zip_code: "Zip Code: " + str(zip_code), 'country': 'Country: ' + str(country)}
+
 
 def get_api_key():
     config = configparser.ConfigParser()
     config.read("config.ini")
     return config['openweathermap']['API_KEY']
+
 
 def get_weather_results(zip_code, country_code, api_key):
     api_url = "https://api.openweathermap.org/data/2.5/weather?q={},{}&appid={}".format(zip_code, country_code, api_key)
@@ -13,5 +32,7 @@ def get_weather_results(zip_code, country_code, api_key):
     response = requests.get(api_url)
     return response.json()
 
+
 if __name__ == '__main__':
-    print(get_weather_results("87000", 'FR', get_api_key()))
+    app.run()
+    # print(get_weather_results("87000", 'FR', get_api_key()))
